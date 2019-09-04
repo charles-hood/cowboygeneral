d1e07b94a5a83cb01c400d871ef0b9bfa19bd364
Charles Hood <charles.hood@gmail.com>
2019-09-03 22:50:57 -0400

    Update PageFooter.js

--- src/components/PageFooter.js
+++ src/components/PageFooter.js
@@ -5,7 +5,7 @@ export default function PageFooter() {
   return (
     <footer id="footer">
       <ul className="copyright">
-        <li>&copy; {config.authorName}</li>
+        <li>&copy; 2019 {config.authorName}</li>
       </ul>
     </footer>
   );
